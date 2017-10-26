026cf3c99206cb0c0ca510b938f00383d9bfdee4
hasan fakhra <hasanfakhra@hasans-MacBook-Air.local>
2017-10-26 17:29:06 +0100

    first commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,26 @@
+
+
+
+import Foundation
+
+
+class Question {
+    
+    
+    
+    let questionText : String
+    
+    let answer : Bool
+    
+    
+    
+    init(text : String, correctAnswer: Bool){
+        
+        questionText = text
+        answer = correctAnswer
+        
+    }
+    
+    
+    
+}
